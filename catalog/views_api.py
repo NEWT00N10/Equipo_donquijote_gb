4c00ac1cd5cd156ae8fc4e27dcbb80e7a3c22de7
NEWT00N10 <220300794@ucaribe.edu.mx>
2025-05-10 05:57:50 -0500

    Feat: búsqueda en vivo con autosuggest en navbar

--- catalog/views_api.py
+++ catalog/views_api.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from rest_framework import viewsets, permissions
 from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
@@ -7,15 +8,24 @@ from catalog.serializers import BookSerializer
 from catalog.filters import BookFilter
 
 
+
+
 class BookViewSet(viewsets.ReadOnlyModelViewSet):
     """
     Listado público con filtros avanzados.
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    #permission_classes = [permissions.AllowAny]
 
-    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,          # ← añade SearchFilter
+    ]
+    filterset_fields = ['genre', 'book_type']
     filterset_class = BookFilter
-    ordering_fields = ["price", "title"]
+    ordering_fields   = ['price', 'title', 'author']
     search_fields = ["title", "author", "isbn_10", "isbn_13"]
+
+
